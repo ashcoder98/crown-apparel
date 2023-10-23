ac071c3283dea6dda295a43c0ce50f637bbba216
Alex Harris <alexharrisemail@gmail.com>
2023-10-23 08:58:40 -0400

    adds logo, routing, and navigation bar styled

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import {Fragment} from "react";
 
 import {Outlet, Link} from "react-router-dom";
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+import './navigation.styles.scss'
 
 const Navigation = () => {
     return (
@@ -8,7 +10,7 @@ const Navigation = () => {
 
             <div className="navigation">
             <Link className="logo-container" to="/">
-            <div>Logo</div>            
+            <CrownLogo className="logo" />            
             </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to='/shop'>
