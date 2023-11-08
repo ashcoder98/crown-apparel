17da0bb41aa1e9933e666b24ab9641b6247c4cc2
Alex Harris <alexharrisemail@gmail.com>
2023-11-07 20:55:32 -0500

    adds sign in component that can create new user in firebase storage

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
-
+import {initializeApp} from 'firebase/app';
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -10,16 +10,41 @@ const firebaseConfig = {
     storageBucket: "crown-apparel-db-2eafb.appspot.com",
     messagingSenderId: "128871751275",
     appId: "1:128871751275:web:2a92c4ea3b50a31cc49b2e"
-  };
-  
-  // Initialize Firebase
+};
+
+// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
-    prompt: "select_account"
-});
+provider.setCustomParameters({prompt: "select_account"});
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+    const userSnapShot = await getDoc(userDocRef);
+    console.log(userSnapShot);
+    console.log(userSnapShot.exists());
+
+    if (!userSnapShot.exists()){
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+      } catch (error) {
+        console.log('error creating the user', error.message);
+      }
+    }
+    return userDocRef;
+};
