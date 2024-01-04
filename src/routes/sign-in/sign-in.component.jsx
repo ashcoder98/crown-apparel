1e74fb98a932e70b082ca3425e8fce0c144e61d8
Alex <alexharrisemail@gmail.com>
2024-01-03 21:47:54 -0500

    starting sign in portion of sign in page

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,18 @@
-
-
-import { signInWithGooglePopup, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
+import {signInWithGooglePopup, createUserDocumentFromAuth} from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import FormInput from "../../components/form-input/form-input.component";
 
+import { useState } from "react";
 const SignIn = () => {
-  
+
+const defaultFormFields = {
+    email: '',
+    password: ''
+};
+    
+const [formFields, setFormFields] = useState(defaultFormFields);
+
 
 
     const logGoogleUser = async() => {
@@ -13,10 +20,14 @@ const SignIn = () => {
         const userDocRef = await createUserDocumentFromAuth(user);
     };
 
+
+    
     return (
         <div>
             <h1>Sign In Page</h1>
+         
             <button onClick={logGoogleUser}>Sign In with Google Popup</button>
+               
             <SignUpForm></SignUpForm>
         </div>
     )
